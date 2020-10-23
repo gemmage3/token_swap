56d515bb583d9b0ab6c8c5ccb64429e64a0a2267
gemma gonputh <gemmage3@hotmail.co.uk>
2020-10-23 12:23:17 +0100

    Project Set-Up

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,10 @@
+const Token = artifacts.require("Token");
+const EthSwap = artifacts.require("EthSwap");
+
+module.exports = function(deployer) {
+  deployer.deploy(Token);
+
+  deployer.deploy(EthSwap);
+};
+
+
